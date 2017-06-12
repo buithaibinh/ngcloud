4032660453e073e6160bc7e3aabac2d692bd5c34
Bui Van Binh <bvbinh@sk-global.biz>
2017-06-12 18:22:15 +0700

    inline

--- src/app/containers/tables/inline-table/inline-table.component.ts
+++ src/app/containers/tables/inline-table/inline-table.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { ContactDataService, ContactData } from '../../../shared/services/contact.service';
+
 @Component({
   selector: 'app-inline-table',
   templateUrl: './inline-table.component.html',
   styleUrls: ['./inline-table.component.css']
 })
 export class InlineTableComponent implements OnInit {
-
-  constructor() { }
-
+  editing = {};
+  rows = [];
+  constructor(private contactDataService: ContactDataService) {
+    this.contactDataService.getAll().subscribe((data: any[]) => {
+      this.rows = data;
+    });
+  }
   ngOnInit() {
+  }
+
+  updateValue(event, cell, cellValue, row) {
+    this.editing[row.$$index + '-' + cell] = false;
+    this.rows[row.$$index][cell] = event.target.value;
   }
 
 }
--- src/app/containers/tables/pinning-table/pinning-table.component.ts
+++ src/app/containers/tables/pinning-table/pinning-table.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { ContactDataService, ContactData } from '../../../shared/services/contact.service';
+
 @Component({
   selector: 'app-pinning-table',
   templateUrl: './pinning-table.component.html',
   styleUrls: ['./pinning-table.component.css']
 })
 export class PinningTableComponent implements OnInit {
-
-  constructor() { }
+  rows = [];
+  constructor(private contactDataService: ContactDataService) {
+    this.contactDataService.getAll().subscribe((data: any[]) => {
+      this.rows = data;
+    });
+  }
 
   ngOnInit() {
   }
-
 }
